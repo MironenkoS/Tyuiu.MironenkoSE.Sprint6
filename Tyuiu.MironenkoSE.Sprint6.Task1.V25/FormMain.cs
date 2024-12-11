27e12586a24b3dd7b21f27b3a60d835276c2896e
664 <stephencurrytopone@gmail.com>
2024-12-11 22:31:49 +0500

    Task 1

--- Tyuiu.MironenkoSE.Sprint6.Task1.V25/FormMain.cs
+++ Tyuiu.MironenkoSE.Sprint6.Task1.V25/FormMain.cs
@@ -16,38 +16,7 @@ namespace Tyuiu.MironenkoSE.Sprint6.Task1.V25
         {
             InitializeComponent();
         }
-        DataService ds = new DataService();
-
-        private void buttonDone_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                int startStep = Convert.ToInt32(textBoxVarA_LVA.Text);
-                int stopStep = Convert.ToInt32(textBoxVarB_LVA.Text);
-                string strLine;
-                int len = ds.GetMassFunction(startStep, stopStep).Length;
-                double[] valueArray;
-                valueArray = new double[len];
-                valueArray = ds.GetMassFunction(startStep, stopStep);
-                textBoxAnswer_LVA.Text = "";
-                textBoxAnswer_LVA.AppendText("+----------+----------+" + Environment.NewLine);
-                textBoxAnswer_LVA.AppendText("|    X     |   f(x)   |" + Environment.NewLine);
-                textBoxAnswer_LVA.AppendText("+----------+----------+" + Environment.NewLine);
-
-                for (int i = 0; i <= len - 1; i++)
-                {
-                    strLine = String.Format("|{0,5:d}     | {1, 5:f2}    |", startStep, valueArray[i]);
-                    textBoxAnswer_LVA.AppendText(strLine + Environment.NewLine);
-                    startStep++;
-
-                }
-
-                textBoxAnswer_LVA.AppendText("+----------+----------+" + Environment.NewLine);
-
-            }
-            catch
-            {
-                MessageBox.Show("Ââåäåíû íåâåðíûå äàííûå", "Îøèáêà", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        
             }
         }
 
