3bfd6e0a9ae6a12149a87c2ffd5aecfe23f41665
664 <stephencurrytopone@gmail.com>
2024-12-12 22:02:38 +0500

    Task 5

--- Tyuiu.MironenkoSE.Sprint6.Task5.V6.Lib/DataService.cs
+++ Tyuiu.MironenkoSE.Sprint6.Task5.V6.Lib/DataService.cs
@@ -9,24 +9,27 @@ namespace Tyuiu.MironenkoSE.Sprint6.Task5.V6.Lib
             using (StreamReader reader = new StreamReader(path))
             {
                 string line;
-                while ((line = reader.ReadLine()!) != null)
+                while ((line = reader.ReadLine()) != null)
                 {
                     len++;
                 }
             }
 
             double[] numsArray = new double[len];
+
             int index = 0;
-            using (StreamReader reader2 = new StreamReader(path))
+            using (StreamReader reader = new StreamReader(path))
             {
                 string line;
-                while ((line = reader2.ReadLine()!) != null)
+                while ((line = reader.ReadLine()) != null)
                 {
                     numsArray[index] = Convert.ToDouble(line);
                     index++;
                 }
             }
-            numsArray = numsArray.Where(val => val >= 10).ToArray();
+
+            numsArray = numsArray.Where(val => val > 0).ToArray();
+
             return numsArray;
         }
     }
