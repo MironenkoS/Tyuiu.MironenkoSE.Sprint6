301b5209df5dd793e7be2a70bea551d960845ab1
664 <stephencurrytopone@gmail.com>
2024-12-13 00:03:30 +0500

    Task 7

---
+++ Tyuiu.MironenkoSE.Sprint6.Task7.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MironenkoSE.Sprint6.Task7.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
