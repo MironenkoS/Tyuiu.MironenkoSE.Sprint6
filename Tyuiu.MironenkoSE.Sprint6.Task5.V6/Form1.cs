be5cbd61dddb5541d0ebec2de43e14c95c42bb43
664 <stephencurrytopone@gmail.com>
2024-12-12 23:53:24 +0500

    Task 6

--- Tyuiu.MironenkoSE.Sprint6.Task5.V6/Form1.cs
+++ Tyuiu.MironenkoSE.Sprint6.Task5.V6/Form1.cs
@@ -11,7 +11,40 @@ namespace Tyuiu.MironenkoSE.Sprint6.Task5.V6
         string path = @"C:\Users\664\source\repos\Tyuiu.MironenkoSE.Sprint6\Sprint6Task6\InPutDataFileTask5V6.txt";
         private void Form1_Load(object sender, EventArgs e)
         {
+            dataGridViewOutPut_KRV.ColumnCount = 2;
+            dataGridViewOutPut_KRV.Columns[0].Width = 20;
+            dataGridViewOutPut_KRV.Columns[1].Width = 50;
 
+            this.chartDiagr_KRV.ChartAreas[0].AxisX.Title = "Ось X";
+            this.chartDiagr_KRV.ChartAreas[0].AxisY.Title = "Ось Y";
+
+            chartDiagr_KRV.Series[0].Points.Clear();
+
+            double[] numsMass = new double[ds.len];
+
+            numsMass = ds.LoadFromDataFile(path);
+
+            for (int i = 0; i < numsMass.Length; i++)
+            {
+                dataGridViewOutPut_KRV.Rows.Add(Convert.ToString(i), Convert.ToString(numsMass[i]));
+                chartDiagr_KRV.Series[0].Points.AddXY(i, numsMass[i]);
+            }
+        }
+
+        private void buttonOpen_KRV_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process txt = new System.Diagnostics.Process();
+            txt.StartInfo.FileName = "notepad.exe";
+            txt.StartInfo.Arguments = path;
+            txt.Start();
+        }
+
+        private void buttonWiki_KRV_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 5 выполнил студент группы СМАРТб-23-1 Хасанов Ренат Владиславович", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
         }
     }
 }
---
+++ Tyuiu.MironenkoSE.Sprint6.Task6.V3.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.MironenkoSE.Sprint6.Task6.V3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
