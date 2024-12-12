82ce75fe271c99dcf0706d5efab1b6faae890396
664 <stephencurrytopone@gmail.com>
2024-12-12 19:32:05 +0500

    Task 4

---
+++ Tyuiu.MironenkoSE.Sprint6.Task4.V8.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.MironenkoSE.Sprint6.Task4.V8.Lib
+{
+    public class DataService : ISprint6Task4V8
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int count = 0;
+            int len = Math.Abs(startValue) + Math.Abs(stopValue) + 1;
+            double[] array = new double[len];
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x + 2 != 0)
+                {
+                    double y = (Math.Sin(x) + (Math.Cos(x) + 1) / (2 - x) + 2 * x);
+                    y = Math.Round(y, 2);
+                    array[count] = y;
+                    count++;
+                }
+                else
+                {
+                    double y = 0;
+                    array[count] = y;
+                    count++;
+                }
+
+            }
+            return array;
+        }
+    }
+}
---
+++ Tyuiu.MironenkoSE.Sprint6.Task4.V8/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.MironenkoSE.Sprint6.Task4.V8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
